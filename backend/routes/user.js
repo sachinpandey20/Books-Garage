1d9b281d2c6edb56e33fd1a27bd09718fca0777b
Sachin Pandey <sachipandey19.sp@gmail.com>
2025-11-20 00:49:50 +0530

    fixed bcrypt issue

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/user");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { authenticateToken }  = require("./userAuth");
 require("dotenv").config();
